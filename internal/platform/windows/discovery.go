d4353b27ee19fbc1dcbf67107902100e2f161acd
Malcolm GetAHead <malcolm@getahead.local>
2026-04-29 00:08:16 -0400

    S6-02: Refactor discovery response from fmt.Sprintf to struct-based xml.Marshal
    Replace hand-rolled string template with proper Go struct XML marshaling.
    Generates unique ActivityId per request (uuid.New()).
    Same semantic output: SOAP 1.2 envelope, WS-Addressing headers, no trailing
    slash on enrollment namespace, EnrollmentVersion 4.0, OnPremise auth.

--- internal/platform/windows/discovery.go
+++ internal/platform/windows/discovery.go
@@ -3,13 +3,21 @@ package windows
 import (
 	"encoding/xml"
 	"fmt"
+
+	"github.com/google/uuid"
 )
 
 // MS-MDE2 discovery namespace and action URIs
 const (
-	DiscoveryNS       = "http://schemas.microsoft.com/windows/management/2012/01/enrollment"
-	DiscoveryAction   = "http://schemas.microsoft.com/windows/management/2012/01/enrollment/IDiscoveryService/Discover"
+	DiscoveryNS        = "http://schemas.microsoft.com/windows/management/2012/01/enrollment"
+	DiscoveryAction    = "http://schemas.microsoft.com/windows/management/2012/01/enrollment/IDiscoveryService/Discover"
 	DiscoverRespAction = "http://schemas.microsoft.com/windows/management/2012/01/enrollment/IDiscoveryService/DiscoverResponse"
+
+	soapNS       = "http://www.w3.org/2003/05/soap-envelope"
+	addressingNS = "http://www.w3.org/2005/08/addressing"
+	activityNS   = "http://schemas.microsoft.com/2004/09/ServiceModel/Diagnostics"
+	xsiNS        = "http://www.w3.org/2001/XMLSchema-instance"
+	xsdNS        = "http://www.w3.org/2001/XMLSchema"
 )
 
 // DiscoverRequest represents the inner Discover element within the SOAP body.
@@ -44,6 +52,50 @@ type discoverSOAPBody struct {
 	Discover DiscoverRequest `xml:"Discover"`
 }
 
+// --- Response structs for xml.Marshal ---
+
+type discoverRespEnvelope struct {
+	XMLName xml.Name          `xml:"s:Envelope"`
+	XMLNS_S string            `xml:"xmlns:s,attr"`
+	XMLNS_A string            `xml:"xmlns:a,attr"`
+	Header  discoverRespHdr   `xml:"s:Header"`
+	Body    discoverRespBody  `xml:"s:Body"`
+}
+
+type discoverRespHdr struct {
+	Action     discoverRespAction `xml:"a:Action"`
+	ActivityId discoverActivityId `xml:"ActivityId"`
+	RelatesTo  string             `xml:"a:RelatesTo"`
+}
+
+type discoverRespAction struct {
+	MustUnderstand string `xml:"s:mustUnderstand,attr"`
+	Value          string `xml:",chardata"`
+}
+
+type discoverActivityId struct {
+	XMLNS string `xml:"xmlns,attr"`
+	Value string `xml:",chardata"`
+}
+
+type discoverRespBody struct {
+	XMLNS_XSI        string            `xml:"xmlns:xsi,attr"`
+	XMLNS_XSD        string            `xml:"xmlns:xsd,attr"`
+	DiscoverResponse discoverRespInner `xml:"DiscoverResponse"`
+}
+
+type discoverRespInner struct {
+	XMLNS          string         `xml:"xmlns,attr"`
+	DiscoverResult discoverResult `xml:"DiscoverResult"`
+}
+
+type discoverResult struct {
+	AuthPolicy                 string `xml:"AuthPolicy"`
+	EnrollmentVersion          string `xml:"EnrollmentVersion"`
+	EnrollmentPolicyServiceUrl string `xml:"EnrollmentPolicyServiceUrl"`
+	EnrollmentServiceUrl       string `xml:"EnrollmentServiceUrl"`
+}
+
 // ParseDiscoverRequest parses a SOAP 1.2 envelope containing a Discover request.
 // Windows sends discovery inside <s:Envelope> with WS-Addressing headers.
 func ParseDiscoverRequest(data []byte) (*DiscoverRequest, string, error) {
@@ -62,26 +114,38 @@ func ParseDiscoverRequest(data []byte) (*DiscoverRequest, string, error) {
 // GenerateDiscoverResponse generates a SOAP 1.2 envelope discovery response
 // with proper WS-Addressing headers per MS-MDE2 spec.
 func GenerateDiscoverResponse(enrollmentURL, policyURL, relatesToMessageID string) ([]byte, error) {
-	resp := fmt.Sprintf(`<?xml version="1.0" encoding="UTF-8"?>
-<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope"
-   xmlns:a="http://www.w3.org/2005/08/addressing">
-  <s:Header>
-    <a:Action s:mustUnderstand="1">%s</a:Action>
-    <ActivityId xmlns="http://schemas.microsoft.com/2004/09/ServiceModel/Diagnostics">d9eb2fdd-e38a-46ee-bd93-aea9dc86a3b8</ActivityId>
-    <a:RelatesTo>%s</a:RelatesTo>
-  </s:Header>
-  <s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-     xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-    <DiscoverResponse xmlns="%s">
-      <DiscoverResult>
-        <AuthPolicy>OnPremise</AuthPolicy>
-        <EnrollmentVersion>4.0</EnrollmentVersion>
-        <EnrollmentPolicyServiceUrl>%s</EnrollmentPolicyServiceUrl>
-        <EnrollmentServiceUrl>%s</EnrollmentServiceUrl>
-      </DiscoverResult>
-    </DiscoverResponse>
-  </s:Body>
-</s:Envelope>`, DiscoverRespAction, relatesToMessageID, DiscoveryNS, policyURL, enrollmentURL)
+	env := discoverRespEnvelope{
+		XMLNS_S: soapNS,
+		XMLNS_A: addressingNS,
+		Header: discoverRespHdr{
+			Action: discoverRespAction{
+				MustUnderstand: "1",
+				Value:          DiscoverRespAction,
+			},
+			ActivityId: discoverActivityId{
+				XMLNS: activityNS,
+				Value: uuid.New().String(),
+			},
+			RelatesTo: relatesToMessageID,
+		},
+		Body: discoverRespBody{
+			XMLNS_XSI: xsiNS,
+			XMLNS_XSD: xsdNS,
+			DiscoverResponse: discoverRespInner{
+				XMLNS: DiscoveryNS,
+				DiscoverResult: discoverResult{
+					AuthPolicy:                 "OnPremise",
+					EnrollmentVersion:          "4.0",
+					EnrollmentPolicyServiceUrl: policyURL,
+					EnrollmentServiceUrl:       enrollmentURL,
+				},
+			},
+		},
+	}
 
-	return []byte(resp), nil
+	data, err := xml.MarshalIndent(env, "", "  ")
+	if err != nil {
+		return nil, fmt.Errorf("marshal discovery response: %w", err)
+	}
+	return append([]byte(xml.Header), data...), nil
 }
